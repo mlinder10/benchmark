f16d48b22a3d2e06771b6e6b54156ed2518bc922
Matt Linder <linder2015@outlook.com>
2024-04-20 00:36:47 -0400

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import useMonday from "./useMonday";
 
 export default function App() {
-  const { theme, func } = useMonday();
+  const { theme, func, settings } = useMonday();
 
-  return <main className={theme}>{func}</main>;
+  if (settings === null) return <>Loading...</>;
+
+  const cUnit = settings.unit.custom_unit;
+  const symbol = settings.unit.symbol;
+  const text =
+    settings.unit.direction === "right"
+      ? `${func}${cUnit === "" ? symbol : cUnit}`
+      : `${cUnit === "" ? symbol : cUnit}${func}`;
+
+  return <main className={theme}>{text}</main>;
 }
