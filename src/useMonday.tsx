6ae6ebff92bfdcd6fbf346e9fe84a102c35cc5d0
Matt Linder <linder2015@outlook.com>
2024-04-19 20:08:37 -0400

    upload

--- src/useMonday.tsx
+++ src/useMonday.tsx
@@ -4,44 +4,49 @@ import { monday } from "./constants";
 
 export default function useMonday() {
   const [theme, setTheme] = useState<Theme>("light");
+  const [boardIds, setBoardIds] = useState<number | undefined>(undefined);
 
   useEffect(() => {
     monday.listen("context", (data) => {
       console.log(data);
       setTheme(data.data.theme as Theme);
       const unchecked = data.data as any;
-      queryBoards(unchecked.boardIds)
+      setBoardIds(unchecked.boardIds);
     });
   }, []);
 
-  async function queryBoards(boardIds: number | undefined) {
-    if (!boardIds) return;
-    try {
-      const res = await monday.api(`
-            query {
-                boards(ids: ${JSON.stringify(boardIds)}) {
-                    id
-                    items_page {
-                        items {
-                            group {
-                                id
-                            }
-                            column_values(types: [numbers, mirror, formula]) {
-                                id
-                                text
-                                value
-                                type
+  useEffect(() => {
+    async function queryBoards() {
+      if (!boardIds) return;
+      try {
+        const res = await monday.api(`
+                query {
+                    boards(ids: ${JSON.stringify(boardIds)}) {
+                        id
+                        items_page {
+                            items {
+                                group {
+                                    id
+                                }
+                                column_values(types: [numbers, mirror, formula]) {
+                                    id
+                                    text
+                                    value
+                                    type
+                                }
                             }
                         }
                     }
                 }
-            }
-        `);
-      console.log(res);
-    } catch (err) {
-      console.log(err);
+            `);
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
     }
-  }
+
+    queryBoards();
+  }, [boardIds]);
 
   return { theme };
 }
